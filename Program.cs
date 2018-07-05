7d6573abb60f0fe80a8ca9bf90ba97e284d5515f
akihiro-shimada <akihiro.shimada.jz@hitachi-solutions.com>
2018-07-05 14:54:41 +0900

    add ,

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
